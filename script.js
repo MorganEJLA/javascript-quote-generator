cbb84acc7637b15d26171e8681ecb3f16d47b712
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-03-28 18:12:30 -0400

    reformatted files, made buttons work

---
+++ script.js
@@ -0,0 +1,72 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+// get quotes from API//
+//loading spinner shown//
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+
+}
+//loading spinner hidden//
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+function newQuote(){
+    loading();
+    // pick a random quote from the array //
+    // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    // console.log(quote);
+    if(!quote.author){
+        authorText.textContent = "Unknown";
+    }else{
+        authorText.textContent = quote.author;
+    }
+    //Check Quote Length //
+    //add the long-quote class in the media query to dynamically change the font depending on 
+    // length
+    if(quote.text.length > 120){
+        quoteText.classList.add("long-quote");
+    }else {
+        quoteText.classList.remove("long-quote");
+    }
+  
+    quoteText.textContent = quote.text;
+    complete();
+}
+async function getQuotes(){
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try{
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      newQuote();
+    } catch(error){
+        getQuotes();
+        //Catch error Here
+    }
+}
+
+// // On Load run getQuotes function //
+
+//function to tweet a quote//
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//onload
+
+// newQuote();
+// loading();
+getQuotes();
